ec1d9c3410c89c9189532a65382d58fc025cc234
gu1dx <guadybenitez@gmail.com>
2023-06-08 11:02:08 -0300

    finished adding animations to User Clicks

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ $(".btn").click(function(){
 
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 function nextSequence(){
@@ -16,19 +17,22 @@ function nextSequence(){
     
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     
     playSound(randomChosenColour);
 }
 
-function playSound(name){
-    
+function playSound(name){    
     var audio = new Audio("sounds/" + name + ".mp3");    
     audio.play();
 }
 
 function animatePress(currentColour){
-    $(currentColour).addClass(pressed);
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+      }, 100)
 }
 
 
