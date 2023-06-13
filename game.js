6583268f4aaab52205b78a10beba2fdaabd7a1f3
gu1dx <guadybenitez@gmail.com>
2023-06-13 08:47:17 -0300

    added files to start the game when a key is pressed

--- game.js
+++ game.js
@@ -1,6 +1,16 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern =[];
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+    if (!started){
+        $("#level-title").text("Level" + level);
+        nextSequence();
+        started = true;
+    }    
+});
 
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
@@ -11,6 +21,8 @@ $(".btn").click(function(){
 });
 
 function nextSequence(){
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
